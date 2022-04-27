39021c4989389d5e3c1b65461342fd97be46dc93
atiglla <99499241+atiglla@users.noreply.github.com>
2022-04-27 14:59:35 -0500

    [TAREA] sensei

--- Ninja.js
+++ Ninja.js
@@ -1,28 +1,40 @@
 class Ninja {
-    constructor(nombre){
+    constructor(nombre,salud,velocidad,fuerza){
         this.nombre=nombre;
-        this.salud=100;
-        this.velocidad=3;
-        this.fuerza=3;
-
+        this.salud=salud || 100;
+        this.velocidad=velocidad||3;
+        this.fuerza=fuerza||3;
     }
-
     sayName(){
         console.log(`Your name is ${this.nombre}`);
     }
-
     showStats(){
-        console.log(`El nombre es ${this.nombre} , tiene de ${this.fuerza} fuerza, su velocidad es ${this.velocidad} y la salud del Ninja es ${this.salud}.`);
+        console.log(`Nombre: ${this.nombre}, Salud: ${this.salud}, Velocidad:${this.velocidad}, Fuerza: ${this.fuerza}.`);
     }
-
     drinkSake (){
         this.salud+=10;
     }
-
 }
 
+class Sensei extends Ninja{
+    constructor(nombre){
+        super(nombre,200,10,10);
+        this.sabiduria=10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log(`Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.`)
+    }
+}
 
 
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador pued e hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
